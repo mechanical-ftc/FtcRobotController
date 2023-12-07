86cef0bf5a63fc21704f36d8574448aa5e48d57c
tylerpaul85 <tylerpaul85@gmail.com>
2023-12-06 19:02:34 -0600

    added 4964 code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/RandomizationScoring.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+//import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//
+//import java.util.jar.Attributes;
+//
+//@Autonomous(name = "RandomizationTesting")
+//public class RandomizationScoring extends LinearOpMode {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//}
